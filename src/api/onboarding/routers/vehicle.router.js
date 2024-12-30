ed276e31456914c58e794b155af5859e0ef0688e
himanshuj0311 <himanshu.masai@gmail.com>
2024-12-30 16:23:20 +0530

    Check commit

--- src/api/onboarding/routers/vehicle.router.js
+++ src/api/onboarding/routers/vehicle.router.js
@@ -573,7 +573,6 @@ router.get("/getDocument", async (req, res) => {
   getDocument(req, res);
 })
 
-// get All Document
 router.get("/getAllDocument", async (req, res) => {
   getAllDocument(req, res);
   // await Log({
