6854fc542940dd4692aca277f427071084a241a6
himanshuj0311 <himanshu.masai@gmail.com>
2025-02-13 10:40:32 +0530

    Minor changes

--- src/api/onboarding/models/timeline.model.js
+++ src/api/onboarding/models/timeline.model.js
@@ -1,4 +1,7 @@
 const Timeline = require("../../../db/schemas/onboarding/timeline.schema");
+const {whatsappMessage} = require ("../../../utils/whatsappMessage");
+const {sendEmailForExtendOrVehicleChange} = require ("../../../utils/emailSend");
+const Booking = require ("../../../db/schemas/onboarding/booking.schema")
 
 const timelineFunction = async (req, res) => {
   // function convertDate(inputDate) {
@@ -57,13 +60,34 @@ if(isStart && isStart===true)
         });
       }
 
+      const booking = await Booking.findOne({ _id: currentBooking_id })
+      .populate('userId', 'firstName contact email')
+      .populate('stationMasterUserId', 'contact')
+      .select('stationMasterUserId userId');
+    
       const updatedTimeline = [...existingData.timeLine, ...timeLine];
 
-      await Timeline.updateOne(
-        { currentBooking_id },
-        { $set: { timeLine: updatedTimeline } }
-      );
+      // await Timeline.updateOne(
+      //   { currentBooking_id },
+      //   { $set: { timeLine: updatedTimeline } }
+      // );
 
+
+  if(timeLine[timeLine.length-1].PaymentLink && timeLine[timeLine.length-1].PaymentLink !=="")
+    {const link = `https://${timeLine[timeLine.length-1].PaymentLink}`;
+    const amount = timeLine[timeLine.length-1].paymentAmount
+    const flag = (timeLine[timeLine.length-1].changeToVehicle == "") ? "Extend vehicle" : "Change vehicle";
+    const firstName = booking.userId.firstName;
+    const email = booking.userId.email;
+    const contact = booking.userId.contact;
+    const managerContact = booking.stationMasterUserId.contact
+    const bookingId = existingData.bookingId
+
+    const messageData=[firstName, flag, bookingId, amount, link, managerContact ];
+
+    whatsappMessage(contact,"booking_payment_link",messageData)
+    sendEmailForExtendOrVehicleChange(email,firstName, flag, bookingId, amount, link, managerContact)
+}
       return res.status(200).json({
         status: 200,
         message: "Timeline updated successfully",
