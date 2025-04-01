0e12d28d1dae00f8ecb41beec4c66d42630599e5
Lalit Kakkar <78905908+kakkar2@users.noreply.github.com>
2025-04-01 17:47:39 +0530

    Update cron.js

--- src/utils/cron.js
+++ src/utils/cron.js
@@ -14,6 +14,7 @@ async function cancelPendingPayments() {
       {
         paymentStatus: "pending",
         createdAt: { $lte: oneHourAgo },
+        paymentMethod: { $ne: "cash" }
       },
       {
         $set: {
