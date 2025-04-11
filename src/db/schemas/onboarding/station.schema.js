c67bd94b3d7b61a8db0fa0e1fbf00a3c96462047
Lalit Kakkar <78905908+kakkar2@users.noreply.github.com>
2025-04-11 18:14:26 +0530

    Update station.schema.js

--- src/db/schemas/onboarding/station.schema.js
+++ src/db/schemas/onboarding/station.schema.js
@@ -1,86 +1,75 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const stationSchema = new Schema({
-   
+const stationSchema = new Schema(
+  {
     stationId: {
       type: String,
       required: true,
-      unique: true
+      unique: true,
     },
     locationId: {
       type: Schema.Types.ObjectId,
-      ref: 'Location',
-      required: true
+      ref: "Location",
+      required: true,
     },
-    userId: {
-      type: Schema.Types.ObjectId,
-      ref: 'User'
-    },
+    userId: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "User",
+      },
+    ],
     stationName: {
       type: String,
-      required: true
+      required: true,
+      lowercase: true,
     },
     address: {
       type: String,
-      required: true
+      required: true,
+      lowercase: true,
     },
     city: {
       type: String,
-      required: true
+      required: true,
+      lowercase: true,
     },
     state: {
       type: String,
-      required: true
+      required: true,
+      lowercase: true,
     },
-  
+
     pinCode: {
       type: Number,
-      required: true
+      required: true,
     },
     country: {
       type: String,
-      required: true
+      required: true,
+      lowercase: true,
     },
     openStartTime: {
       type: Number,
       required: true,
-      default:9
+      default: 9,
     },
     openEndTime: {
       type: Number,
       required: true,
-      default:21
+      default: 21,
     },
-   
+
     latitude: {
-      type: String
+      type: String,
     },
     longitude: {
-      type: String
-    }
-  }, { timestamps: { createdAt: 'createdAt', updatedAt: 'updatedAt' } });
+      type: String,
+    },
+  },
+  { timestamps: { createdAt: "createdAt", updatedAt: "updatedAt" } }
+);
 
-  stationSchema.pre('save', function (next) {
-    if (this.stationName) {
-      this.stationName = this.stationName.toLowerCase(); 
-    }
-    if (this.country) {
-      this.country = this.country.toLowerCase(); 
-    }
-    if (this.state) {
-      this.state = this.state.toLowerCase(); 
-    }
-    if (this.city) {
-      this.city = this.city.toLowerCase(); 
-    }
-   
-    next();
-  });
-  
-  const station = mongoose.model('station', stationSchema);
-  
-  module.exports = station;
-  
+const station = mongoose.model("station", stationSchema);
 
-  
+module.exports = station;
