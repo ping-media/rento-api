4b96350e47ffec11347f5ab0149b659585a21fae
Lalit Kakkar <78905908+kakkar2@users.noreply.github.com>
2025-04-28 16:51:22 +0530

    Update vehicle-master.schema.js

--- src/db/schemas/onboarding/vehicle-master.schema.js
+++ src/db/schemas/onboarding/vehicle-master.schema.js
@@ -1,48 +1,37 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const vehicleMasterSchema = new Schema({
+const vehicleMasterSchema = new Schema(
+  {
     vehicleImage: {
-        type: String,
-        required: true
+      type: String,
+      required: true,
     },
     vehicleBrand: {
-        type: String,
-        required: true
+      type: String,
+      required: true,
+      trim: true,
+      lowercase: true,
     },
     vehicleName: {
-        type: String,
-        required: true,
-        unique: true
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+      lowercase: true,
     },
     imageFileName: {
-        type: String,
-        
+      type: String,
     },
     vehicleType: {
-        enum: ["gear", "non-gear"],
-        type: String,
-        required: true
-    }, 
-    
-}, { timestamps: { createdAt: 'createdAt', updatedAt: 'updatedAt' } });
+      enum: ["gear", "non-gear"],
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: { createdAt: "createdAt", updatedAt: "updatedAt" } }
+);
 
-vehicleMasterSchema.pre('save', function (next) {
-    if (this.vehicleName) {
-      this.vehicleName = this.vehicleName.toLowerCase(); 
-    }
-    if (this.vehicleBrand) {
-      this.vehicleBrand = this.vehicleBrand.toLowerCase(); 
-    }
-    if(this.vehicleType){
-        this.vehicleType = this.vehicleType.toLowerCase();
-    }
-    next();
-  });
-
-
-const vehicleMaster = mongoose.model('vehicleMaster', vehicleMasterSchema);
+const vehicleMaster = mongoose.model("vehicleMaster", vehicleMasterSchema);
 
 module.exports = vehicleMaster;
-
-
