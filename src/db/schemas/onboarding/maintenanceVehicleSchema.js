c60b7ae6d177790e951e52a8474ebede6126cfdf
Lalit Kakkar <78905908+kakkar2@users.noreply.github.com>
2025-04-28 16:48:22 +0530

    Update maintenanceVehicleSchema.js

--- src/db/schemas/onboarding/maintenanceVehicleSchema.js
+++ src/db/schemas/onboarding/maintenanceVehicleSchema.js
@@ -1,26 +1,35 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-// Define the MaintenanceVehicle Schema
 const maintenanceVehicleSchema = new Schema(
   {
- vehicleTableId:{
-    type: Schema.Types.ObjectId,
-       required: true,
-       ref :"vehicleTable"
- },
- startDate:{
-        type:String,
-        require:true,
- },
+    vehicleTableId: {
+      type: Schema.Types.ObjectId,
+      required: true,
+      ref: "vehicleTable",
+    },
+    startDate: {
+      type: String,
+      require: true,
+    },
 
- endDate:{
-        type:String,
-        require:true,
- }
+    endDate: {
+      type: String,
+      require: true,
+    },
+    reason: {
+      type: String,
+      default: "general maintenance",
+      require: true,
+      lowercase: true,
+    },
+  },
+  { timestamps: { createdAt: "createdAt", updatedAt: "updatedAt" } }
+);
 
-  }, { timestamps: { createdAt: 'createdAt', updatedAt: 'updatedAt' } });
+const MaintenanceVehicle = mongoose.model(
+  "MaintenanceVehicle",
+  maintenanceVehicleSchema
+);
 
-  const MaintenanceVehicle = mongoose.model('MaintenanceVehicle', maintenanceVehicleSchema);
-  
-  module.exports = MaintenanceVehicle;
+module.exports = MaintenanceVehicle;
