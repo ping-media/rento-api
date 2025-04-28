1432adc4ab7674cca9d6106fdf7f9849e2f8dd5e
Lalit Kakkar <78905908+kakkar2@users.noreply.github.com>
2025-04-28 16:55:30 +0530

    Update vehicle.schema.js

--- src/db/schemas/onboarding/vehicle.schema.js
+++ src/db/schemas/onboarding/vehicle.schema.js
@@ -1,52 +1,46 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const VehicleSchema = new Schema({
-  bookingCount: {
-    type: Number,
-    required: true
+const VehicleSchema = new Schema(
+  {
+    bookingCount: {
+      type: Number,
+      required: true,
+    },
+    pricePerday: {
+      type: Number,
+      required: true,
+    },
+    name: {
+      type: String,
+      required: true,
+    },
+    url: {
+      type: String,
+      required: true,
+    },
+    distanceLimit: {
+      type: Number,
+      required: true,
+    },
+    accessChargePerKm: {
+      type: Number,
+      required: true,
+    },
+    transmissionType: {
+      type: String,
+      required: true,
+    },
+    brand: {
+      type: String,
+      required: true,
+    },
   },
-  pricePerday: {
-    type: Number,
-    required: true
-  },
-  name: {
-    type: String,
-    required: true
-  },
-  url: {
-    type: String,
-    required: true
-  },
-  distanceLimit: {
-    type: Number,
-    required: true
-  },
-  accessChargePerKm: {
-    type: Number,
-    required: true
-  },
-  transmissionType: {
-    type: String,
-    required: true
-  },
-  brand: {
-    type: String,
-    required: true
-  },
-  createdAt: {
-    type: Date,
-    default: Date.now
-  },
-  updatedAt: {
-    type: Date,
-    default: Date.now
-  }
-}, { timestamps: { createdAt: 'createdAt', updatedAt: 'updatedAt' } });
-
+  { timestamps: true }
+);
 
 // Pre-save middleware to convert fields to lowercase
-VehicleSchema.pre('save', function (next) {
+VehicleSchema.pre("save", function (next) {
   if (this.name) {
     this.name = this.name.toLowerCase(); // Convert name to lowercase
   }
@@ -56,6 +50,6 @@ VehicleSchema.pre('save', function (next) {
   next();
 });
 
-const Vehicle = mongoose.model('Vehicle', VehicleSchema);
+const Vehicle = mongoose.model("Vehicle", VehicleSchema);
 
 module.exports = Vehicle;
